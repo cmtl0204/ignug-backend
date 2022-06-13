b8d37543525bc57789c72f1c5f098aa433a84b14
cmtl0204 <cesar.tamayo0204@gmail.com>
2022-06-13 14:52:38 -0500

    add users services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './controllers/users.controller';
 import { ProductsController } from './controllers/products.controller';
+import { UsersService } from './services/users.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UsersController, ProductsController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
