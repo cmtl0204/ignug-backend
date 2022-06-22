3d083670efe85e10db2d44f504aca89861b9a525
cmtl0204 <cesar.tamayo0204@gmail.com>
2022-06-22 13:42:29 -0500

    fix bugs

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('products')
+@Entity('product')
 export class ProductEntity {
   @PrimaryGeneratedColumn()
   id: number;
