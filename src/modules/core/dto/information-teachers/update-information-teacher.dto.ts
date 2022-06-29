9350dee7688935a42a07ba27fe29eb1a5b4ee2be
MEJIA MOROCHO ZAIDA STEPHANIA <1752212777>
2022-06-29 12:21:16 -0500

    teacher

---
+++ src/modules/core/dto/information-teachers/update-information-teacher.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateInformationTeacherDto } from './create-information-teacher.dto';
+
+export class UpdateInformationTeacherDto extends PartialType(CreateInformationTeacherDto) {}
--- src/modules/core/dto/teachers/update-teacher.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTeacherDto } from '@core/dto';
-
-export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {}
