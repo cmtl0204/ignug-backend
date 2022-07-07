dfbb8512a5a4c0860cb96aae81e55c6e4d48ad6f
MEJIA MOROCHO ZAIDA STEPHANIA <1752212777>
2022-07-06 20:15:56 -0500

    mejora

--- src/modules/core/controllers/information-teachers.controller.ts
+++ src/modules/core/controllers/information-teachers.controller.ts
@@ -11,50 +11,67 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { CreateInformationTeacherDto} from '@core/dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import {
-  CreateInformationTeacherDto,
-  UpdateInformationTeacherDto,
-} from '@core/dto';
 import { InformationTeachersService } from '@core/services';
 
 @ApiTags('information-teachers')
-@Controller('information-teachers')
+@Controller('informationTeachers')
 export class InformationTeachersController {
   constructor(private informationTeachersService: InformationTeachersService) {}
 
   @ApiOperation({ summary: 'Crea un nuevo docente' })
-  @Post('')
+  @Post()
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() payload: CreateInformationTeacherDto) {
     const data = await this.informationTeachersService.create(payload);
 
     return {
       data,
-      message: `create`,
+      message: `created`,
     };  
-  }
-
-  @ApiOperation({ summary: 'Elimina un docente' })
-  @Delete(':id')
-  @HttpCode(HttpStatus.OK)
-  async remove(@Param('id', ParseIntPipe) id: number) {
-    const data = await this.informationTeachersService.remove(id);
-    return {
-      data,
-      message: `deleted`,
-    };
   }
 
   @ApiOperation({ summary: 'Consulta los docentes' })
-  @Get('')
+  @Get()
   @HttpCode(HttpStatus.OK)
   async findAll(@Query() params: any) {
+    const sortFields = params.sort
+      ? params.sort.split(',').filter((sort) => sort != '')
+      : null;
+    const selectedFields = params.fields
+      ? params.fields.split(',').filter((field) => field != '')
+      : null;
     const data = await this.informationTeachersService.findAll();
     return {
       data,
-      message: `findall`,
+      message: `index`,
     };  
+  }
+
+  @Get('catalogue')
+  @HttpCode(HttpStatus.OK)
+  catalogue(@Query() params: any) {
+    const selectedFields = params.fields
+      ? params.fields.split(',').filter((field) => field != '')
+      : null;
+    return {
+      data: 'data',
+      message: `catalogue`,
+    };
+  }
+
+  @ApiOperation({ summary: 'Filtrar usuarios' })
+  @Get('filter')
+  @HttpCode(HttpStatus.OK)
+  filter(@Query() params: any) {
+    const search = params.search
+      ? params.search.split(',').filter((search) => search != '')
+      : null;
+    return {
+      data: 'data',
+      message: `filter`,
+    };
   }
 
   @ApiOperation({ summary: 'Consulta un solo docente' })
@@ -65,20 +82,30 @@ export class InformationTeachersController {
     return {
       data,
       message: `show ${id}`,
-    };  }
-
+    };  
+  }
 
   @ApiOperation({ summary: 'Actualiza un la informacion del docente' })
   @Put(':id')
   @HttpCode(HttpStatus.CREATED)
-  async update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() payload: UpdateInformationTeacherDto,
-  ) {
+  async update(@Param('id', ParseIntPipe) id: number, @Body() payload: any) {
     const data = await this.informationTeachersService.update(id, payload);
     return {
       data,
-      message: `updated ${id}`,
+      message: `informationTeacher updated ${id}`,
     };
   }
+
+  @ApiOperation({ summary: 'Elimina un docente' })
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    const data = await this.informationTeachersService.remove(id);
+    return {
+      data,
+      message: `informationTeacher deleted ${id}`,
+    };
+  }
+
+ 
 }
