c9836d653f5facf6ce29e5a9010e9fa89cd48418
Curipoma <at.curipoma@yavirac.edu.ec>
2022-07-08 12:53:40 -0500

    add filter

--- src/modules/core/services/institutions.service.ts
+++ src/modules/core/services/institutions.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import {
+  DeleteResult,
+  FindOptionsWhere,
+  ILike,
+  LessThan,
+  Repository,
+} from 'typeorm';
 import { CreateInstitutionDto, UpdateInstitutionDto } from '@core/dto';
 import { CataloguesService } from '@core/services';
 import { InstitutionEntity } from '@core/entities';
+import { FilterInstitutionDto } from '../dto/institutions/filter-institution.dto';
 
 @Injectable()
 export class InstitutionsService {
@@ -24,7 +31,10 @@ export class InstitutionsService {
     return await this.institutionRepository.save(newInstitution);
   }
 
-  async findAll(): Promise<InstitutionEntity[]> {
+  async findAll(params?: FilterInstitutionDto): Promise<InstitutionEntity[]> {
+    if (params.limit && params.offset) {
+      return this.pagination(params.limit, params.offset);
+    }
     return await this.institutionRepository.find();
   }
 
@@ -57,5 +67,53 @@ export class InstitutionsService {
 
   async remove(id: number): Promise<DeleteResult> {
     return await this.institutionRepository.softDelete(id);
+  }
+
+  pagination(limit: number, offset: number) {
+    return this.institutionRepository.find({
+      relations: ['bloodType', 'gender'],
+      take: limit,
+      skip: offset,
+    });
+  }
+
+  filter(params: FilterInstitutionDto) {
+    const where: FindOptionsWhere<InstitutionEntity>[] = [];
+
+    const { search } = params;
+
+    if (search) {
+      where.push({ acronym: ILike(`%${search}`) });
+      where.push({ cellphone: ILike(`%${search}`) });
+      where.push({ code: ILike(`%${search}`) });
+      where.push({ codeSniese: ILike(`%${search}`) });
+      where.push({ denomination: ILike(`%${search}`) });
+      where.push({ email: ILike(`%${search}`) });
+      where.push({ logo: ILike(`%${search}`) });
+      where.push({ name: ILike(`%${search}`) });
+      where.push({ phone: ILike(`%${search}`) });
+      where.push({ shortName: ILike(`%${search}`) });
+      where.push({ slogan: ILike(`%${search}`) });
+      where.push({ web: ILike(`%${search}`) });
+    }
+
+    return this.institutionRepository.find({
+      relations: ['bloodType', 'gender'],
+      where,
+    });
+  }
+
+  filterByBirthdate(birthdate: Date) {
+    const where: FindOptionsWhere<InstitutionEntity> = {};
+    console.log(birthdate);
+    if (birthdate) {
+      // where.birthdate = LessThan(birthdate);
+    }
+
+    console.log(where);
+    return this.institutionRepository.find({
+      relations: ['bloodType', 'gender'],
+      where,
+    });
   }
 }
