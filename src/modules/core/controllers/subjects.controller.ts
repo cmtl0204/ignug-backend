d780b8fd6740641ef1347bb98f6f6678ddc3ae17
Odalis Rea <68356142+Odalis26@users.noreply.github.com>
2022-07-12 11:30:08 -0500

    subjects

--- src/modules/core/controllers/subjects.controller.ts
+++ src/modules/core/controllers/subjects.controller.ts
@@ -7,17 +7,17 @@ import {
   HttpStatus,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Put,
   Query,
 } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import {
-  CreateSubjectDto,
-  UpdateSubjectDto,
-  FilterSubjectDto,
-} from '@core/dto';
 import { SubjectsService } from '@core/services';
+import { CreateSubjectDto, FilterSubjectDto, UpdateSubjectDto } from '@core/dto';
+import { SubjectEntity } from '@core/entities';
+import { ResponseHttpModel } from '@exceptions';
+
 
 @ApiTags('subjects')
 @Controller('subjects')
@@ -39,21 +39,15 @@ export class SubjectsController {
   @Get()
   @HttpCode(HttpStatus.OK)
   async findAll(@Query() params: FilterSubjectDto) {
-    const data = await this.subjectsService.findAll(params);
-    //const sortFields = params.sort
-    // ? params.sort.split(',').filter((sort) => sort != '')
-    //: null;
-    //const selectedFields = params.fields
-    // ? params.fields.split(',').filter((field) => field != '')
-    // : null;
-    //const data = await this.subjectsService.findAll();
+    const response = await this.subjectsService.findAll(params);
     return {
-      data,
+      data: response.data,
+      pagination: response.pagination,
       message: `index`,
     };
   }
 
-  @ApiOperation({ summary: 'View one subjects' })
+  @ApiOperation({ summary: 'Find Subject' })
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   async findOne(@Param('id', ParseIntPipe) id: number) {
@@ -61,10 +55,11 @@ export class SubjectsController {
     return {
       data,
       message: `show ${id}`,
-    };
+      title: `Success`,
+    }as ResponseHttpModel;
   }
 
-  @ApiOperation({ summary: 'Update subjects' })
+  @ApiOperation({ summary: 'Update Subject' })
   @Put(':id')
   @HttpCode(HttpStatus.CREATED)
   async update(
@@ -72,20 +67,37 @@ export class SubjectsController {
     @Body() payload: UpdateSubjectDto,
   ) {
     const data = await this.subjectsService.update(id, payload);
+    
     return {
       data: data,
-      message: `subject updated ${id}`,
-    };
+      message: `Subject updated ${id}`,
+      title: `Updated`,
+    } as ResponseHttpModel;
   }
 
-  @ApiOperation({ summary: 'Remove subjects' })
+  @ApiOperation({ summary: 'Remove Subject' })
   @Delete(':id')
   @HttpCode(HttpStatus.CREATED)
   async remove(@Param('id', ParseIntPipe) id: number) {
     const data = await this.subjectsService.remove(id);
+   
     return {
       data,
-      message: `subject deleted ${id}`,
-    };
+      message: `Subject deleted ${id}`,
+      title: `Deleted`,
+    } as ResponseHttpModel;
+  }
+
+  @ApiOperation({ summary: 'Remove All SubjectS' })
+  @Patch('remove-all')
+  @HttpCode(HttpStatus.CREATED)
+  async removeAll(@Body() payload: SubjectEntity[]) {
+    const data = await this.subjectsService.removeAll(payload);
+
+    return {
+      data,
+      message: `Subjects deleted`,
+      title: `Deleted`,
+    } as ResponseHttpModel;
   }
 }
--- src/modules/core/services/subjects.service.ts
+++ src/modules/core/services/subjects.service.ts
@@ -1,13 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CataloguesService, CurriculaService } from '@core/services';
-import { FindOptionsWhere, ILike, LessThan, Repository } from 'typeorm';
-import {
-  CreateSubjectDto,
-  UpdateSubjectDto,
-  FilterSubjectDto,
-} from '@core/dto';
+import { Repository, FindOptionsWhere, ILike, LessThan } from 'typeorm';
+import {CreateSubjectDto, UpdateSubjectDto, FilterSubjectDto} from '@core/dto';
 import { SubjectEntity } from '@core/entities';
+import { PaginationDto } from '@core/dto';
+import { CataloguesService, CurriculaService } from '@core/services';
 
 @Injectable()
 export class SubjectsService {
@@ -20,6 +17,7 @@ export class SubjectsService {
 
   async create(payload: CreateSubjectDto) {
     const newSubject = this.subjectRepository.create(payload);
+
     newSubject.academicPeriod = await this.catalogueService.findOne(
       payload.academicPeriod.id,
     );
@@ -30,50 +28,64 @@ export class SubjectsService {
     newSubject.curriculum = await this.curriculumService.findOne(
       payload.curriculum.id,
     );
-    const response = await this.subjectRepository.save(newSubject);
-    return await this.subjectRepository.save(response);
+
+    const subjectCreated = await this.subjectRepository.save(newSubject);
+
+    return await this.subjectRepository.save(subjectCreated);
+  }
+
+  async catalogue() {
+    const data = await this.subjectRepository.findAndCount({
+      relations: ['academicPeriod', 'curriculum', 'state', 'type'],
+      take: 1000,
+    });
+
+    return { pagination: { totalItems: data[1], limit: 10 }, data: data[0] };
   }
 
   async findAll(params?: FilterSubjectDto) {
-    //Pagination
-    if (params.limit && params.page) {
-      return this.pagination(params.limit, params.page);
+   //Pagination & Filter by search
+    if (params) {
+      return await this.paginateAndFilter(params);
     }
 
-    //Filter by search
-    if (params.search) {
-      return this.filter(params);
+    //Other filters
+    if (params.scale) {
+      return this.filterByScale(params.scale);
     }
 
-    return await this.subjectRepository.find({
+    if (params.code) {
+      return this.filterByCode(params.code);
+    }
+
+    //All
+    const data = await this.subjectRepository.findAndCount({
       relations: ['academicPeriod', 'curriculum', 'state', 'type'],
     });
+
+    return { pagination: { totalItems: data[1], limit: 10 }, data: data[0] };
   }
 
   async findOne(id: number) {
     const subject = await this.subjectRepository.findOne({
-      where: {
-        id,
-      },
+      relations: ['academicPeriod', 'curriculum', 'state', 'type'],
+      where: {id},
     });
 
-    if (subject === null) {
-      throw new NotFoundException('La asignatura no se encontro');
+    if (!subject) {
+      throw new NotFoundException('Subject not found');
     }
 
     return subject;
   }
 
   async update(id: number, payload: UpdateSubjectDto) {
-    const subject = await this.subjectRepository.findOne({
-      where: {
-        id,
-      },
-    });
+    const subject = await this.subjectRepository.findOneBy({ id });
 
-    if (subject === null) {
-      throw new NotFoundException('La asignatura no se encontro');
+    if (!subject) {
+      throw new NotFoundException('Subject not found');
     }
+
     subject.academicPeriod = await this.catalogueService.findOne(
       payload.academicPeriod.id,
     );
@@ -87,22 +99,46 @@ export class SubjectsService {
     );
 
     this.subjectRepository.merge(subject, payload);
-    return await this.subjectRepository.save(subject);
+
+    return  this.subjectRepository.save(subject);
   }
 
   async remove(id: number) {
-    const subject = await this.subjectRepository.findOne({
-      where: {
-        id,
-      },
-    });
+    const subject = await this.subjectRepository.findOneBy({ id });
 
-    if (subject === null) {
-      throw new NotFoundException('La asignatura no se encontro');
+    if (!subject) {
+      throw new NotFoundException('Subject not found');
     }
 
-    await this.subjectRepository.softDelete(id);
-    return true;
+    return await this.subjectRepository.softRemove(subject);
+  }
+
+  async removeAll(payload: SubjectEntity[]) {
+    return await this.subjectRepository.softRemove(payload);
+  }
+
+  private async paginateAndFilter(params: FilterSubjectDto) {
+    let where: FindOptionsWhere<SubjectEntity> | FindOptionsWhere<SubjectEntity>[];
+    where = {};
+    let { page, search } = params;
+    const { limit } = params;
+
+    if (search) {
+      search = search.trim();
+      page = 0;
+      where = [];
+      where.push({ code: ILike(`%${search}%`) });
+      where.push({ name: ILike(`%${search}%`) });
+    }
+
+    const data = await this.subjectRepository.findAndCount({
+      relations: ['academicPeriod', 'curriculum', 'state', 'type'],
+      where,
+      take: limit,
+      skip: PaginationDto.getOffset(limit, page),
+    });
+
+    return { pagination: { limit, totalItems: data[1] }, data: data[0] };
   }
 
   pagination(limit: number, offset: number) {
@@ -129,31 +165,33 @@ export class SubjectsService {
     });
   }
 
-  filterByScale(scale: number) {
+  private async filterByScale(scale: number) {
     const where: FindOptionsWhere<SubjectEntity> = {};
-    console.log(scale);
+
     if (scale) {
       where.scale = LessThan(scale);
     }
 
-    console.log(where);
-    return this.subjectRepository.find({
+    
+    const data = await this.subjectRepository.findAndCount({
       relations: ['academicPeriod', 'curriculum', 'state', 'type'],
-      where,
     });
+
+    return { pagination: { limit: 10, totalItems: data[1] }, data: data[0] };
   }
 
-  filterByCode(code: string) {
+  private async filterByCode(code: string) {
     const where: FindOptionsWhere<SubjectEntity> = {};
-    console.log(code);
+
     if (code) {
       where.code = LessThan(code);
     }
 
-    console.log(where);
-    return this.subjectRepository.find({
+    const data = await this.subjectRepository.findAndCount({
       relations: ['academicPeriod', 'curriculum', 'state', 'type'],
       where,
     });
+
+    return { pagination: { limit: 10, totalItems: data[1] }, data: data[0] };
   }
 }
