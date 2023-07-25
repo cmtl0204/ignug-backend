9f8131de8f25511d53fe90a70bbe448d70eca796
1755221270 <dam.carrera@yavira.edu.ec>
2023-07-25 18:56:56 -0500

    Update Students

--- src/modules/core/dto/information-students/create-information-student.dto.ts
+++ src/modules/core/dto/information-students/create-information-student.dto.ts
@@ -7,76 +7,77 @@ import {
   Max,
 } from 'class-validator';
 import { CatalogueEntity } from '@core/entities';
+import { isNotEmptyValidationOptions, isNumberValidationOptions, isStringValidationOptions } from '@shared/validation';
 
 export class CreateInformationStudentDto {
-  @IsNotEmpty()
+  @IsNotEmpty(isNotEmptyValidationOptions())
   readonly isExecutedPractice: CatalogueEntity;
 
-  @IsNotEmpty()
+  @IsNotEmpty(isNotEmptyValidationOptions())
   readonly isExecutedCommunity: CatalogueEntity;
 
-  @IsNotEmpty()
+  @IsNotEmpty(isNotEmptyValidationOptions())
   readonly isDisability: CatalogueEntity;
 
-  @IsNotEmpty()
+  @IsNotEmpty(isNotEmptyValidationOptions())
   readonly isLostGratuity: CatalogueEntity;
 
-  @IsNotEmpty()
+  @IsNotEmpty(isNotEmptyValidationOptions())
   readonly isSubjectRepeat: CatalogueEntity;
 
-  @IsString({ message: 'Debe ser un string' })
+  @IsString(isStringValidationOptions())
   @MaxLength(1000, { message: 'Maximo 1000 caracteres' })
   readonly address: string;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   @Min(0, { message: 'El número de digitos mínimo es 0.' })
   readonly community: number;
 
-  @IsString({ message: 'Debe ser un string' })
+  @IsString(isStringValidationOptions())
   @MaxLength(255, { message: 'Maximo 255 caracteres' })
   readonly contactEmergencyName: string;
 
-  @IsString({ message: 'Debe ser un string' })
+  @IsString(isStringValidationOptions())
   @MaxLength(255, { message: 'Maximo 255 caracteres' })
   readonly contactEmergencyKinship: string;
 
-  @IsString({ message: 'Debe ser un string' })
+  @IsString(isStringValidationOptions())
   @MaxLength(255, { message: 'Maximo 255 caracteres' })
   readonly contactEmergencyPhone: string;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({},isNumberValidationOptions())
   @Min(0, { message: 'El número de digito mínimo es 0.' })
   @Max(100, { message: 'Maximo 100 digito' })
   readonly disabilityPercentage: number;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   @Min(0, { message: 'El número de digito mínimo es 0.' })
   readonly educationalAmount: number;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   @Min(0, { message: 'El número de digito mínimo es 0.' })
   readonly economicAmount: number;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   readonly familyIncome: number;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   @Min(1, { message: 'El número de digitos mínimo es 1.' })
   @Max(20, { message: 'Maximo 20 digitos' })
   readonly membersHouseNumber: number;
 
-  @IsString({ message: 'Debe ser un string' })
+  @IsString(isStringValidationOptions())
   @MaxLength(100, { message: 'Maximo 100 caracteres' })
   readonly postalCode: string;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   @Min(0, { message: 'El número de digito mínimo es 0.' })
   readonly practiceHours: number;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({},isNumberValidationOptions())
   readonly scholarshipAmount: number;
 
-  @IsNumber({}, { message: 'Debe ser tipo numero' })
+  @IsNumber({}, isNumberValidationOptions())
   @Min(0, { message: 'El número de digito mínimo es 0.' })
   @Max(100, { message: 'Maximo 100 digito' })
   readonly tariffScholarshipPercentage: number;
