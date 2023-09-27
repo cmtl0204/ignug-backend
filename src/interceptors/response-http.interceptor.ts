27b4d55379c760171178b72fb79423582a4cd86d
Cesar Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2023-09-27 16:11:11 -0500

    fix bugs

--- src/interceptors/response-http.interceptor.ts
+++ src/interceptors/response-http.interceptor.ts
@@ -1,29 +1,29 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, BadRequestException, ServiceUnavailableException } from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { environments } from '../environments';
+import {Injectable, NestInterceptor, ExecutionContext, CallHandler, ServiceUnavailableException} from '@nestjs/common';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {environments} from '../environments';
 
 export interface Response<T> {
-  data: T;
+    data: T;
 }
 
 @Injectable()
 export class ResponseHttpInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-    if (environments.serviceUnavailable) {
-      throw new ServiceUnavailableException();
-    }
+    intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+        if (environments.serviceUnavailable) {
+            throw new ServiceUnavailableException();
+        }
 
-    return next.handle().pipe(
-      map(response => {
-        return {
-          data: response.data,
-          pagination: response.pagination,
-          message: response.message,
-          title: response.title,
-          version: environments.version,
-        };
-      }),
-    );
-  }
+        return next.handle().pipe(
+            map(response => {
+                return {
+                    data: response.data,
+                    pagination: response.pagination,
+                    message: response.message,
+                    title: response.title,
+                    version: environments.version,
+                };
+            }),
+        );
+    }
 }
