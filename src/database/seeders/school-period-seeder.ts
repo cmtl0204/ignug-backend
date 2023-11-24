ba9ac44b29fba3b6865fe0d9a261a761f54fd8b5
Cesar Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2023-11-24 09:43:34 -0500

    fix bugs seeders

--- src/database/seeders/school-period-seeder.ts
+++ src/database/seeders/school-period-seeder.ts
@@ -3,6 +3,8 @@ import {SeedSchoolPeriodDto} from '@core/dto';
 import {CatalogueEntity} from 'src/modules/core/entities/catalogue.entity';
 import {CataloguesService, InstitutionsService, SchoolPeriodsService} from '@core/services';
 import {CatalogueTypeEnum} from '@shared/enums';
+import * as XLSX from "xlsx";
+import {join} from "path";
 
 @Injectable()
 export class SchoolPeriodSeeder {
@@ -17,32 +19,39 @@ export class SchoolPeriodSeeder {
 
     private async createSchoolPeriod() {
         const schoolPeriods: SeedSchoolPeriodDto[] = [];
-
         const catalogues = await this.cataloguesService.findCache();
-        const closeState = await catalogues.find((item: CatalogueEntity) => item.code === 'close' && item.type === CatalogueTypeEnum.SCHOOL_PERIODS_STATE);
-        const openState = await catalogues.find((item: CatalogueEntity) => item.code === 'open' && item.type === CatalogueTypeEnum.SCHOOL_PERIODS_STATE);
-
         const institution = ((await this.institutionsService.findAll()).data)[0];
-        for (let i = 1; i <= 10; i++) {
+
+        const workbook = XLSX.readFile(join(process.cwd(), 'src/database/seeders/files/school_periods.xlsx'));
+
+
+        const workbookSheets = workbook.SheetNames;
+        const sheet = workbookSheets[0];
+        const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+
+        for (const item of dataExcel) {
+            const state = catalogues.find((catalogue: CatalogueEntity) => catalogue.code === item['state'] && catalogue.type === CatalogueTypeEnum.SCHOOL_PERIODS_STATE);
+
             const schoolPeriod: SeedSchoolPeriodDto = {
                 institution: institution,
-                code: `cod${i}`,
-                codeSniese: `SP-SN-00${i}`,
+                code: item['code'],
+                codeSniese: item['code'],
                 isVisible: true,
-                name: `Test School Period ${i}`,
-                shortName: `TSP${i}`,
-                startedAt: new Date(`2023-01-${i < 10 ? '0' + i : i}`),
-                endedAt: new Date(`2023-06-${i < 10 ? '0' + i : i}`),
-                ordinaryStartedAt: new Date(`2023-01-${i < 10 ? '0' + i : i}`),
-                ordinaryEndedAt: new Date(`2023-05-${i < 10 ? '0' + i : i}`),
-                extraOrdinaryStartedAt: new Date(`2023-02-${i < 10 ? '0' + i : i}`),
-                extraOrdinaryEndedAt: new Date(`2023-02-${i < 10 ? '0' + i : i}`),
-                especialStartedAt: new Date(`2023-03-${i < 10 ? '0' + i : i}`),
-                especialEndedAt: new Date(`2023-03-${i < 10 ? '0' + i : i}`),
-                state: i === 10 ? openState : closeState,
+                name: item['name'],
+                shortName: item['short_name'],
+                startedAt: item['started_at'],
+                endedAt: item['ended_at'],
+                ordinaryStartedAt: item['ordinary_started_at'],
+                ordinaryEndedAt: item['ordinary_ended_at'],
+                extraOrdinaryStartedAt: item['extraOrdinary_started_at'],
+                extraOrdinaryEndedAt: item['extraOrdinary_ended_at'],
+                especialStartedAt: item['especial_started_at'],
+                especialEndedAt: item['especial_ended_at'],
+                state,
             };
             schoolPeriods.push(schoolPeriod);
         }
+
         for (const item of schoolPeriods) {
             await this.schoolsPeriod.create(item);
         }
