d178725751b757b1717db19c72005a96af20cf11
Cesar Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2024-06-14 11:18:14 -0500

    add document validations

--- src/modules/core/controllers/document-validations.controller.ts
+++ src/modules/core/controllers/document-validations.controller.ts
@@ -13,23 +13,19 @@ import {
     Query
 } from '@nestjs/common';
 import {ApiTags, ApiOperation} from '@nestjs/swagger';
-import {Auth, User} from '@auth/decorators';
-import {UserEntity} from '@auth/entities';
-import {CreateCareerDto, FilterEnrollmentDto, UpdateCareerDto} from '@core/dto';
-import {CareerEntity} from '@core/entities';
 import {
     CareerParallelsService,
-    CareersService,
     EnrollmentsService,
     SubjectsService,
     TeacherDistributionsService
 } from '@core/services';
 import {ResponseHttpModel} from '@shared/models';
+import {DocumentValidationsService} from "../services/document-validations.service";
 
-@ApiTags('Careers')
-@Controller('careers')
+@ApiTags('Document Validations')
+@Controller('document-validations')
 export class DocumentValidationsController {
-    constructor(private readonly careersService: CareersService,
+    constructor(private readonly documentValidationsService: DocumentValidationsService,
                 private readonly enrollmentsService: EnrollmentsService,
                 private readonly subjectsService: SubjectsService,
                 private readonly careerParallelsService: CareerParallelsService,
@@ -38,186 +34,15 @@ export class DocumentValidationsController {
     }
 
     @ApiOperation({summary: 'Create Career'})
-    @Post()
+    @Get('student-card/:studentId')
     @HttpCode(HttpStatus.CREATED)
-    async create(@Body() payload: CreateCareerDto): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.create(payload);
+    async studentCard(@Param('studentId') studentId: string, @Query('schoolPeriodId') careerId: string, @Query('schoolPeriodId') schoolPeriodId: string): Promise<ResponseHttpModel> {
+        const serviceResponse = await this.documentValidationsService.studentCard(studentId, careerId, schoolPeriodId);
 
         return {
             data: serviceResponse,
-            message: 'Carrera fue creada',
-            title: 'Carrera creada',
+            message: 'Student Card',
+            title: 'Document Validation',
         };
     }
-
-    @ApiOperation({summary: 'Find Career'})
-    @Get(':id')
-    @HttpCode(HttpStatus.OK)
-    async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.findOne(id);
-
-        return {
-            data: serviceResponse,
-            message: `Buscar carrera`,
-            title: `Success`,
-        };
-    }
-
-    @ApiOperation({summary: 'Update Career'})
-    @Put(':id')
-    @HttpCode(HttpStatus.CREATED)
-    async update(@Param('id', ParseUUIDPipe) id: string, @Body() payload: UpdateCareerDto): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.update(id, payload);
-
-        return {
-            data: serviceResponse,
-            message: `Carrera fue actualizada`,
-            title: `Carrera actualizada`,
-        };
-    }
-
-    @ApiOperation({summary: 'Hide'})
-    @Patch(':id/hide')
-    @HttpCode(HttpStatus.CREATED)
-    async hide(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.hide(id);
-
-        return {
-            data: serviceResponse,
-            message: `Carrera Oculto`,
-            title: `Ocultado`,
-        };
-    }
-
-    @ApiOperation({summary: 'Reactivate'})
-    @Patch(':id/reactivate')
-    @HttpCode(HttpStatus.CREATED)
-    async reactivate(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.reactivate(id);
-
-        return {
-            data: serviceResponse,
-            message: `Carrera Reactivado`,
-            title: `Reactivado`,
-        };
-    }
-
-    @ApiOperation({summary: 'Delete Career'})
-    @Delete(':id')
-    @HttpCode(HttpStatus.CREATED)
-    async remove(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.remove(id);
-        return {
-            data: serviceResponse,
-            message: `Carrera fue eliminada`,
-            title: `Carrera eliminada`,
-        };
-    }
-
-    @ApiOperation({summary: 'Delete All Careers'})
-    @Patch('remove-all')
-    @HttpCode(HttpStatus.CREATED)
-    async removeAll(@Body() payload: CareerEntity[]): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.removeAll(payload);
-
-        return {
-            data: serviceResponse,
-            message: `Carreras fueron eliminadas`,
-            title: `Carreras eliminadas`,
-        };
-    }
-
-    @ApiOperation({summary: 'Find Career'})
-    @Get(':id/teachers')
-    @HttpCode(HttpStatus.OK)
-    async findTeachersByCareer(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.findTeachersByCareer(id);
-
-        return {
-            data: serviceResponse,
-            message: `Buscar carrera`,
-            title: `Success`,
-        };
-    }
-
-    @ApiOperation({summary: 'Find Curriculums By Career'})
-    @Get(':id/curriculums')
-    @HttpCode(HttpStatus.OK)
-    async findCurriculumsByCareer(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.findCurriculumsByCareer(id);
-        return {
-            data: serviceResponse,
-            message: `Find Curriculums By Career`,
-            title: 'Success',
-        };
-    }
-
-    @ApiOperation({summary: 'Find Careers By Authenticated User'})
-    @Auth()
-    @Get('users/authenticated')
-    @HttpCode(HttpStatus.OK)
-    async findByAuthenticatedUser(@User() user: UserEntity): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careersService.findCareersByAuthenticatedUser(user);
-
-        return {
-            data: serviceResponse,
-            message: 'Find Careers By Authenticated User',
-            title: `Success`,
-        };
-    }
-
-    @ApiOperation({summary: 'Find Subjects By Career'})
-    @Get(':id/subjects')
-    @HttpCode(HttpStatus.OK)
-    async findSubjectsByCareer(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.subjectsService.findSubjectsByCareer(id);
-
-        return {
-            data: serviceResponse,
-            message: `Success`,
-            title: `Success`,
-        };
-    }
-
-    @ApiOperation({summary: 'Find Enrollments By Career'})
-    @Get(':id/enrollments')
-    @HttpCode(HttpStatus.OK)
-    async findEnrollmentsByCareer(@Param('id', ParseUUIDPipe) id: string, @Query() params: FilterEnrollmentDto): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.enrollmentsService.findEnrollmentsByCareer(id, params);
-
-        return {
-            data: serviceResponse.data,
-            pagination: serviceResponse.pagination,
-            message: `Reporte de notas fueron eliminadas`,
-            title: `Reporte de notas eliminadas`,
-        };
-    }
-
-    @ApiOperation({ summary: 'Find TeacherDistributions By Career' })
-    @Get(':id/teacher-distributions/:teacherDistributionId')
-    @HttpCode(HttpStatus.OK)
-    async findTeacherDistributionsByTeacher(@Param('id', ParseUUIDPipe) id: string,
-                                            @Param('teacherDistributionId', ParseUUIDPipe) teacherDistributionId: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.teacherDistributionsService.findTeacherDistributionsByCareer(id,teacherDistributionId);
-
-        return {
-            data: serviceResponse,
-            message: `Success`,
-            title: `Success`,
-        };
-    }
-
-    @ApiOperation({summary: 'Find Enrollments By Career'})
-    @Get(':id/parallels')
-    @HttpCode(HttpStatus.OK)
-    async findParallelsByCareer(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseHttpModel> {
-        const serviceResponse = await this.careerParallelsService.findParallelsByCareer(id);
-
-        return {
-            data: serviceResponse,
-            message: `Success`,
-            title: `Success`,
-        };
-    }
-
 }
--- src/modules/core/services/document-validations.service.ts
+++ src/modules/core/services/document-validations.service.ts
@@ -8,208 +8,54 @@ import {
     SeedCareerDto,
     UpdateCareerDto,
 } from '@core/dto';
-import {CareerEntity, CatalogueEntity, CurriculumEntity, TeacherEntity} from '@core/entities';
-import {AuthRepositoryEnum, CoreRepositoryEnum, MessageEnum} from '@shared/enums';
+import {CareerEntity, CatalogueEntity, CurriculumEntity, StudentEntity, TeacherEntity} from '@core/entities';
+import {
+    AuthRepositoryEnum,
+    CatalogueEnrollmentStateEnum,
+    CatalogueTypeEnum,
+    CoreRepositoryEnum,
+    MessageEnum
+} from '@shared/enums';
 import {ServiceResponseHttpModel} from '@shared/models';
 import {UserEntity} from '@auth/entities';
+import {CataloguesService} from "./catalogues.service";
 
 @Injectable()
 export class DocumentValidationsService {
     constructor(
-        @Inject(CoreRepositoryEnum.CAREER_REPOSITORY) private repository: Repository<CareerEntity>,
+        @Inject(CoreRepositoryEnum.STUDENT_REPOSITORY) private studentRepository: Repository<StudentEntity>,
         @Inject(AuthRepositoryEnum.USER_REPOSITORY) private userRepository: Repository<UserEntity>,
+        private readonly cataloguesService: CataloguesService,
     ) {
     }
 
-    async catalogue(): Promise<ServiceResponseHttpModel> {
-        const response = await this.repository.findAndCount({
-            relations: {institution: true, modality: true, state: true, type: true},
-            take: 1000,
-        });
+    async studentCard(studentId: string, careerId: string, schoolPeriodId: string) {
+        const catalogues = await this.cataloguesService.findCache();
+        const enrollmentStateEnrolled = catalogues.find(catalogue => catalogue.code === CatalogueEnrollmentStateEnum.ENROLLED && catalogue.type === CatalogueTypeEnum.ENROLLMENT_STATE);
 
-        return {
-            pagination: {
-                totalItems: response[1],
-                limit: 1000,
+        const student = await this.studentRepository.findOne({
+            relations: {
+                informationStudent: {town: true, indigenousNationality: true},
+                enrollment: {career: true},
+                user: {
+                    ethnicOrigin: true,
+                    sex: true,
+                    residenceAddress: {
+                        province: true,
+                        canton: true,
+                    },
+                },
             },
-            data: response[0],
-        };
-    }
-
-    async create(payload: CreateCareerDto): Promise<CareerEntity> {
-        const newEntity: CareerEntity = this.repository.create(payload);
-        const user = this.userRepository.create({id: payload.userId});
-        newEntity.users = [user];
-
-        return await this.repository.save(newEntity);
-    }
-
-    async findAll(): Promise<ServiceResponseHttpModel> {
-        //All
-        const data = await this.repository.findAndCount({
-            relations: ['institution', 'modality', 'state', 'type'],
-        });
-
-        return {pagination: {totalItems: data[1], limit: 10}, data: data[0]};
-    }
-
-    async findCareersByInstitution(institutionId: string, params?: FilterCareerDto): Promise<ServiceResponseHttpModel> {
-        const relations = {modality: true, state: true, type: true};
-        //Pagination & Filter by Search
-        if (params?.limit > 0 && params?.page >= 0) {
-            return await this.paginateAndFilter(institutionId, params, relations);
-        }
-
-        //All
-        const data = await this.repository.findAndCount({
-            where: {institution: {id: institutionId}},
-            relations,
-        });
-
-        return {pagination: {totalItems: data[1], limit: 10}, data: data[0]};
-    }
-
-    async findOne(id: string): Promise<CareerEntity> {
-        const entity = await this.repository.findOne({
-            relations: ['institution', 'modality', 'state', 'type'],
             where: {
-                id,
+                id: studentId,
+                enrollments: {careerId, schoolPeriodId, enrollmentStates: {stateId: enrollmentStateEnrolled.id}},
             },
         });
 
-        if (!entity) {
-            throw new NotFoundException(`La carrera con id:  ${id} no se encontró`);
+        if (!student) {
+            throw new NotFoundException('Estudiante no encontrado');
         }
 
-        return entity;
-    }
-
-    async findByCode(code: string): Promise<CareerEntity> {
-        const entity = await this.repository.findOne(
-            {
-                relations: {curriculums: true},
-                where: {code},
-            });
-
-        if (!entity) {
-            throw new NotFoundException(`La carrera no se encontró`);
-        }
-
-        return entity;
-    }
-
-    async update(id: string, payload: UpdateCareerDto): Promise<CareerEntity> {
-        const entity = await this.repository.findOneBy({id});
-
-        if (!entity) {
-            throw new NotFoundException(MessageEnum.NOT_FOUND);
-        }
-
-        this.repository.merge(entity, payload);
-
-        return await this.repository.save(entity);
-    }
-
-    async remove(id: string): Promise<CareerEntity> {
-        const entity = await this.repository.findOneBy({id});
-
-        if (!entity) {
-            throw new NotFoundException(`La carrera con id:  ${id} no se encontro`);
-        }
-
-        return await this.repository.softRemove(entity);
-    }
-
-    async removeAll(payload: CareerEntity[]): Promise<CareerEntity[]> {
-        return await this.repository.softRemove(payload);
-    }
-
-    private async paginateAndFilter(institutionId: string, params: FilterCareerDto, relations: any): Promise<ServiceResponseHttpModel> {
-        let where: FindOptionsWhere<CareerEntity> | FindOptionsWhere<CareerEntity>[];
-        where = {institution: {id: institutionId}};
-        let {page, search} = params;
-        const {limit} = params;
-
-        if (search) {
-            search = search.trim();
-            page = 0;
-            where = [];
-            where.push({institution: {id: institutionId}, acronym: ILike(`%${search}%`)});
-            where.push({institution: {id: institutionId}, code: ILike(`%${search}%`)});
-            where.push({institution: {id: institutionId}, codeSniese: ILike(`%${search}%`)});
-            where.push({institution: {id: institutionId}, logo: ILike(`%${search}%`)});
-            where.push({institution: {id: institutionId}, name: ILike(`%${search}%`)});
-            where.push({institution: {id: institutionId}, shortName: ILike(`%${search}%`)});
-            where.push({institution: {id: institutionId}, degree: ILike(`%${search}%`)});
-        }
-
-        const response = await this.repository.findAndCount({
-            relations,
-            where,
-            take: limit,
-            skip: PaginationDto.getOffset(limit, page),
-        });
-
-        return {
-            data: response[0],
-            pagination: {limit, totalItems: response[1]},
-        };
-    }
-
-    async hide(id: string): Promise<CareerEntity> {
-        const entity = await this.repository.findOneBy({id});
-
-        if (!entity) {
-            throw new NotFoundException(MessageEnum.NOT_FOUND);
-        }
-        entity.isVisible = false;
-        return await this.repository.save(entity);
-    }
-
-    async reactivate(id: string): Promise<CareerEntity> {
-        const entity = await this.repository.findOneBy({id});
-
-        if (!entity) {
-            throw new NotFoundException(MessageEnum.NOT_FOUND);
-        }
-        entity.isVisible = true;
-        return await this.repository.save(entity);
-    }
-
-    async findTeachersByCareer(id: string): Promise<TeacherEntity[]> {
-        const entity = await this.repository.findOne({
-            relations: {teachers: {user: true}},
-            where: {
-                id,
-            },
-        });
-
-        if (!entity) {
-            throw new NotFoundException(`La carrera con id:  ${id} no se encontró`);
-        }
-
-        return entity.teachers;
-    }
-
-    async findCurriculumsByCareer(id: string): Promise<CurriculumEntity[]> {
-        const entity = await this.repository.findOne({
-            relations: {curriculums: true},
-            where: {
-                id,
-            },
-        });
-
-        if (!entity) {
-            throw new NotFoundException(`La carrera con id:  ${id} no se encontró`);
-        }
-
-        return entity.curriculums;
-    }
-
-    async findCareersByAuthenticatedUser(user: UserEntity): Promise<CareerEntity[]> {
-        return await this.repository.find({
-            relations: {state: true, curriculums: true},
-            where: {users: {id: user.id}},
-        });
+        return student;
     }
 }
