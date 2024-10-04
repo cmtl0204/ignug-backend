dd1e0c5a18bd5fcbf5b400fdf6b65f91dc87e660
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-10-04 13:16:38 -0500

    change version

--- src/interceptors/response-http.interceptor.ts
+++ src/interceptors/response-http.interceptor.ts
@@ -21,7 +21,7 @@ export class ResponseHttpInterceptor<T> implements NestInterceptor<T, Response<T
                     pagination: response.pagination,
                     message: response.message,
                     title: response.title,
-                    version: environments.appVersion,
+                    version: '2.2.2',
                 };
             }),
         );
